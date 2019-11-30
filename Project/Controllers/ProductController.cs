1f674b095108a99839ab6cb67585bb420730ad31
nbozovic16 <nbozovic16@raf.rs>
2019-11-30 16:58:34 +0100

    First part of project

---
+++ Project/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //View result
+        public ActionResult ViewAll()
+        {
+            return View(GetAllProduct()); //passing function int view
+        }
+
+        //collection of products
+        IEnumerable<Product>GetAllProduct()
+        {
+            using (DBModel db = new DBModel()) //db ref.
+            {
+                return db.Products.ToList<Product>(); //covnert list to product list
+            }
+        }
+
+        //Insert-edit operation
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            Product prd = new Product();
+            return View(prd);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit()
+        {
+            return View();
+        }
+    }
+}
